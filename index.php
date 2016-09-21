8840f3d2a8dd6de352e92145f7b4bdc15567e17e
pequenyaja <Usuari@PCAULA167.aules.local>
2016-09-21 12:51:45 +0200

    master1

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
     $app = new Slim\App();
 
     $app->get('/', function($request, $response, $args){
-        $response->write("<h1>Hola </h1>");
+        $response->write("<h1>Hola Git! Si no funcionas bien, te desinstalo!!!</h1>");
         return $response;
   });
-  
+
   $app->run();
  ?>
